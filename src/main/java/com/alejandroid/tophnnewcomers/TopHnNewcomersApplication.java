f852a9c0062bd6ca483aaa4ef8600628ac0af893
Alejandro Martinez de Velasco <siropedealex@gmail.com>
2018-02-23 16:17:26 +0100

    Initial commit with basic springboot app

---
+++ src/main/java/com/alejandroid/tophnnewcomers/TopHnNewcomersApplication.java
@@ -0,0 +1,12 @@
+package com.alejandroid.tophnnewcomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopHnNewcomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopHnNewcomersApplication.class, args);
+	}
+}
